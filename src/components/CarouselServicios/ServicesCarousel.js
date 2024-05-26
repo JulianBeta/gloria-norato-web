e8dbd5cdd60560a9216f2f979dd293b5f45cad34
Your Name <carramplancha@gmail.com>
2024-05-26 14:58:30 -0500

    Fotos servicios actualizadas

--- src/components/CarouselServicios/ServicesCarousel.js
+++ src/components/CarouselServicios/ServicesCarousel.js
@@ -4,7 +4,7 @@ import { ImageCarousel } from "./ImageCarousel";
 
 function ServicesCarousel() {
 
-    const imageFilenames = ['servicios1.jpg', 'servicios2.jpg', 'servicios3.jpg', 'servicios4.jpg', 'servicios5.jpg', 'servicios6.jpg']; 
+    const imageFilenames = ['servicios1.jpg', 'servicios2.jpg', 'servicios3.jpg', 'servicios4.jpg', 'servicios5.jpg', 'servicios6.jpg', 'servicios7.jpg', 'servicios8.jpg', 'servicios9.jpg']; 
 
     const [selectedIndex, setSelectedIndex] = useState(0);
 
