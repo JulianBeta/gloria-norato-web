b2fb7bad52eaaa7337c244911640af582f8c1fd9
julianmaciasa <137076111+julianmaciasa@users.noreply.github.com>
2024-06-12 18:11:31 -0500

    12 de junio

--- src/Sections/SectionProducts.js
+++ src/Sections/SectionProducts.js
@@ -6,8 +6,8 @@ import "../styles/sectionProducts.css"
 function SectionProducts() {
     return (
         
-        <div>
-             <div className='circulo'></div>
+        <div className="contenedorsectionproduct">
+             {/* <div className='circulo'></div> */}
              <div className='circulo1'></div>
              <div className='circulo2'></div>
             <h1 className="productos">PRODUCTOS</h1>
