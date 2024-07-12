c53ef43cadf64af2a2e9aa5a57274073d4cf480c
Your Name <carramplancha@gmail.com>
2024-07-12 15:17:56 -0500

    Ajustes espacios, mapa y menu

--- src/components/Maps.js
+++ src/components/Maps.js
@@ -43,7 +43,7 @@ export function SimpleMap(){
 export default SimpleMap;*/
 
 import React from 'react';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { MapContainer, TileLayer, Marker } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import './../styles/SectionContact.css'
 
