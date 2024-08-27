93948a803c0a9a58449e20add8095536920d92ce
JulianBeta <carramplancha@gmail.com>
2024-08-27 14:47:31 -0500

    Imagenes productos y texto actualizado

--- src/components/Carousel/MainCarousel.js
+++ src/components/Carousel/MainCarousel.js
@@ -6,7 +6,7 @@ import IText from './IText.js';
 const MainCarousel = () => { 
 
   const imageFilenames = ['1.png',  '3.png', '4.png', '5.png', '6.png', '7.png', '8.png', '9.png', '10.png', '11.png', '12.png', '13.png', '14.png', '15.png']; 
-  const textFilenames = ['pruebatexto.png', 'champú.png', 'cremamanos.png', 'AcondicionadorHiDratante.png', 'Aceite.png', 'keraschampu.png'];
+  const textFilenames = ['Texto1.png', 'Texto2.png', 'Texto3.png', 'Texto4.png', 'Texto5.png', 'Texto6.png', 'Texto7.png', 'Texto8.png', 'Texto9.png', 'Texto10.png', 'Texto11.png', 'Texto12.png', 'Texto13.png', 'Texto14.png'];
   const [selectedIndex, setSelectedIndex] = useState(0);
 
   const handleImageClick = (index, indextext) => {
